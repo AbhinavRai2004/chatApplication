d4d22c839eefb6fca0dce8fa26b0e9f4b234fbe0
Abhinav <raiabhinav.in@gmail.com>
2024-02-14 02:11:28 +0530

    server code modify

--- Server/server.js
+++ Server/server.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const http = require("http");
+const { Server } = require("socket.io");
 const path = require("path");
+
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server);
+
+//socket.io
+
+io.on("connection", (socket) => {
+  console.log("A user has connected");
+
+  socket.on("message", (message) => {
+    console.log("Received message:", message);
+    io.emit("message", message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user has disconnected");
+  });
+});
 
 app.use(express.json());
 app.get("/", (req, resp) => {
