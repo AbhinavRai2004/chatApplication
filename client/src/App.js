28417c2b46622e991b0426fc9728d8c17042bd13
Abhinav <raiabhinav.in@gmail.com>
2024-02-14 02:10:35 +0530

    client code completed

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import Chat from "./Chat";
+import "./App.css";
+
+const App = () => {
+  const [isHide, setHide] = useState(false);
+
+  function toggle() {
+    setHide((isHide) => !isHide);
+  }
+
+  return (
+    <>
+    <h1>Chat App</h1>
+    <div className="container">
+      {isHide && <Chat />}
+      <button onClick={toggle}>
+        {isHide ? "Close chatting" : "Start chatting"}
+      </button>
+    </div>
+    </>
+  );
+};
+
+export default App;
---
+++ client/src/Chat.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
+import "./Chat.css";
+
+const socket = io("http://localhost:5000", { transports: ["websocket"] });
+
+const Chat = () => {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages([...messages, message]);
+    });
+
+    return () => {
+      socket.off("message");
+    };
+  }, [messages]);
+
+  const handleMessage = () => {
+    if (input.trim() !== "") {
+      socket.emit("message", input);
+      setInput("");
+    }
+  };
+  return (
+    <div>
+      <div className="chat-box">
+        {messages.map((message, index) => (
+          <div key={index} className="message">
+            {message}
+          </div>
+        ))}
+      </div>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Type your message..."
+      />
+      <button onClick={handleMessage}>Send</button>
+    </div>
+  );
+};
+
+export default Chat;
